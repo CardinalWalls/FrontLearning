8dbc5a3956988bfc3b5b4566d638e1fc33be5137
Ian <yetian.su@gmail.com>
2021-04-23 00:39:19 +0800

    finally made it

--- myHomepage/src/dist/index.23420cfa.js
+++ myHomepage/src/dist/index.23420cfa.js
@@ -442,37 +442,77 @@ id) /*: string*/
 }
 
 },{}],"2X3X1":[function(require,module,exports) {
+const $siteList = $('.siteList')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
 const hashMap = xObject || [
-    {logo: 'A', url: 'https://www.acfun.cn'},
-    {logo: 'B', url: 'https://www.bilibili.com'}
+    {icon: 'A', url: 'https://www.acfun.cn'},
+    {icon: 'B', url: 'https://www.bilibili.com'}
   ]
   
-
+const simplifyUrl = (url) => {
+  return url.replace('https://', '')
+    .replace('http://', '')
+    .replace('www.', '')
+    .replace(/\/.*/, '') // 删除 / 开头的内容
+}
+  
 const render = () => {
-    hashMap.forEach((node, index) => {
-      const $li = $(
-        `<li>
-        <div class="site">
-          <div class="logo">${node.logo}</div>
-          <div class="link">${simplifyUrl(node.url)}</div>
-          <div class="close">
-            <svg class="icon">
-              <use xlink:href="#icon-close"></use>
-            </svg>
-          </div>
+  $siteList.find('.tile').remove()
+  let count = 0;
+  console.log(hashMap)
+  
+  hashMap.forEach((node, index) => {   
+    count ++;   
+    let $tempNode = $(
+      `
+      <div class="tile">
+        <div class="icon">${node.icon}</div>
+        <div class="link">${simplifyUrl(node.url)}</div>
+        <div class="remove">
+          X
         </div>
-        </li>`).insertBefore($lastLi)//没有必要搞链表
-      $li.on('click', () => {
-        window.open(node.url)
-      })
-      $li.on('click', '.close', (e) => {
-        e.stopPropagation() // 阻止冒泡
-        hashMap.splice(index, 1)
-        render()
-      })
+      </div>
+      `
+    );
+    $siteList.append($tempNode)
+    $tempNode.on('click', () => {
+      window.open(node.url)
     })
+    $tempNode.on('click', '.remove', (e) => {
+      e.stopPropagation() // 阻止冒泡
+      hashMap.splice(index, 1)
+      render()
+    })
+
+
+  })
+
+  if(count < 7){
+    $siteList.append(
+      `
+      <div id="addButton" class="tile">
+        <div class="icon">+</div>
+        <div class="link">add a new link</div>
+      </div>
+      `
+    )
+    $('#addButton').on('click', () => {
+      let url = window.prompt('请问你要添加的网址是啥？')
+      if (url.indexOf('http') !== 0) {
+        url = 'https://' + url
+      }
+      console.log(url)
+      hashMap.push({
+        icon: simplifyUrl(url)[0].toUpperCase(),
+        url: url
+      })
+      render()
+    })
+    
+    
+  }
+  
   }      
                                     
   render()
